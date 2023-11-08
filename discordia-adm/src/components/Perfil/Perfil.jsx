d4c6eca5c5f1ebbafdf7a70245d6bb9946f866bb
Gustavopvs1 <polen451@gmail.com>
2023-11-07 19:36:03 -0600

    A

--- discordia-adm/src/components/Perfil/Perfil.jsx
+++ discordia-adm/src/components/Perfil/Perfil.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useAuth} from '../../public/Auth/auth'
 
-
-
 export function Perfil() {
   const auth = useAuth()
   return (
      <>
-        <h1>Bienvenido: {auth.user?.username}</h1>
+        <h1>Hola: {auth.user?.username}</h1>
      </>
   )
 }
